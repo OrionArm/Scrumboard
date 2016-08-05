c3a3662b5e91f5e5d887d2d0608dd1582d9bc402
Doomx3 <Doomx3@ya.ru>
2016-08-05 18:40:07 +0300

    change urls and view

--- scrum/settings.py
+++ scrum/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'scrum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(BASE_DIR), 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,6 +70,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'scrum.wsgi.application'
 
--- scrum/urls.py
+++ scrum/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include, url
 from rest_framework.authtoken.views import obtain_auth_token
+from django.views.generic import TemplateView
 from board.urls import router
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^api/token/', obtain_auth_token, name='api-token'),
 	url(r'^api/', include(router.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 ]
