b3124e310ee97966089ade67f03dab3b865cb983
Doomx3 <Doomx3@ya.ru>
2016-08-07 14:49:25 +0300

    refactor LoginView.js

--- board/static/board/js/views.js
+++ board/static/board/js/views.js
@@ -15,47 +15,20 @@ var TemplateView = Backbone.View.extend({
 	}
 	});
 
-var HomepageView = TemplateView.extend({
-	templateName: '#home-template'
-});
-
-var LoginView = TemplateView.extend({
-	id: 'login',
-	templateName: '#login-template',
-	errorTemplate: _.template('<span class="error"><%- msg %></span>'),
+var FormView = TemplateView.extend({
 	events: {
 		'submit form': 'submit'
 	},
-	submit: function (event) {
-		var data = {};
-		event.preventDefault();
-		this.form = $(event.currentTarget);
-		this.clearErrors();
-		data = {
-		username: $(':input[name="username"]', this.form).val(),
-		password: $(':input[name="password"]', this.form).val()
-		},
-		$.post(app.apiLogin, data)
-		.success($.proxy(this.loginSuccess, this))
-		.fail($.proxy(this.loginFailure, this));
+	errorTemplate: _.template('<span class="error"><%- msg %></span>'),
+	clearErrors: function () {
+		$('.error', this.form).remove();
 	},
-
-	loginSuccess: function (data) {
-		app.session.save(data.token);
-		this.trigger('login', data.token);
-	},
-	
-	loginFailure: function (xhr, status, error) {
-		var errors = xhr.responseJSON;
-		this.showErrors(errors);
-	},
-	
 	showErrors: function (errors) {
 		_.map(errors, function (fieldErrors, name) {
 			var field = $(':input[name=' + name + ']', this.form),
 			label = $('label[for=' + field.attr('id') + ']', this.form);
 			if (label.length === 0) {
-				label = $('label', this.form).first();
+			label = $('label', this.form).first();
 			}
 			function appendError(msg) {
 				label.before(this.errorTemplate({msg: msg}));
@@ -63,8 +36,48 @@ var LoginView = TemplateView.extend({
 			_.map(fieldErrors, appendError, this);
 		}, this);
 	},
-	clearErrors: function () {
-		$('.error', this.form).remove();
+	serializeForm: function (form) {
+		return _.object(_.map(form.serializeArray(), function (item) {
+		// Convert object to tuple of (name, value)
+		return [item.name, item.value];
+		}));
+	},
+	submit: function (event) {
+		event.preventDefault();
+		this.form = $(event.currentTarget);
+		this.clearErrors();
+	},
+	failure: function (xhr, status, error) {
+		var errors = xhr.responseJSON;
+		this.showErrors(errors);
+	},
+	done: function (event) {
+		if (event) {
+		event.preventDefault();
+		}
+		this.trigger('done');
+		this.remove();
+	}
+});
+
+var HomepageView = TemplateView.extend({
+	templateName: '#home-template'
+});
+
+var LoginView = FormView.extend({
+	id: 'login',
+	templateName: '#login-template',
+	submit: function (event) {
+		var data = {};
+		FormView.prototype.submit.apply(this, arguments);
+		data = this.serializeForm(this.form);
+		$.post(app.apiLogin, data)
+			.success($.proxy(this.loginSuccess, this))
+			.fail($.proxy(this.failure, this));
+	},
+	loginSuccess: function (data) {
+		app.session.save(data.token);
+		this.done();
 	}
 });
 
